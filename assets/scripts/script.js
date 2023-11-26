3f0e93e29539c85dba90eee4ec81428e3e2ee56b
Arun Subramaniam <arunsubramaniam@Aruns-MacBook-Air.local>
2023-11-26 13:45:04 +0530

    aos and css enhancements

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,68 +1,41 @@
 (function () {
-    "use strict";
-  
-    var carousels = function () {
-      $(".owl-carousel1").owlCarousel({
-        loop: true,
-        center: true,
-        margin: 0,
-        responsiveClass: true,
-        nav: false,
-        responsive: {
-          0: {
-            items: 1,
-            nav: false
-          },
-          680: {
-            items: 2,
-            nav: false,
-            loop: false
-          },
-          1000: {
-            items: 3,
-            nav: true
-          }
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
         }
-      });
-    };
+      }
+    });
+  };
 
-    $(document).scroll(function () {
-        var $nav = $(".navbar.fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-      });
-
-      $('body').scrollspy({ target: '#navbar-example2' })
-
-
-    // Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+  $(document).scroll(function () {
+    var $nav = $(".navbar.fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
-}
+  $('body').scrollspy({ target: '#navbar-example2' })
 
-initMap();
-  
-    (function ($) {
-      carousels();
-    })(jQuery);
-  })();
+  AOS.init();
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
